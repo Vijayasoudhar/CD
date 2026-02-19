508a391893c1cf262c54a876831a5ec0a79ab6a7
VJ <66190038+Vijayprogrammer@users.noreply.github.com>
2026-02-19 06:17:56 +0530

    CD_v1
    Hands-on Continuous Deployment - Loan app

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM python:3.14.3-bookworm
+
+WORKDIR /flask-docker
+
+RUN python3 -m pip install --upgrade pip
+COPY requirements.txt requirements.txt
+RUN pip3 install -r requirements.txt
+
+COPY . .
+
+CMD ["python","-m","flask","run","--host=0.0.0.0"]
---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, request, Response, jsonify
+import pickle
+
+#Loan app
+
+model_pickle = open(r"classifier.pkl", "rb")
+clf = pickle.load(model_pickle)
+
+app = Flask(__name__)
+
+@app.route("/ping", methods=['GET'])
+def ping():
+    return {"message":"Pinging model!!"}
+
+# defining the endpoint which will make the prediction
+@app.route("/prediction", methods=['POST'])
+def prediction():
+    """ Returns loan application status using ML model
+    """
+    loan_req = request.get_json()
+    print(loan_req)
+    if loan_req['Gender'] == "Male":
+        Gender = 0
+    else:
+        Gender = 1
+    if loan_req['Married'] == "Unmarried":
+        Married = 0
+    else:
+        Married = 1
+    if loan_req['Credit_History'] == "Unclear Debts":
+        Credit_History = 0
+    else:
+        Credit_History = 1
+
+    ApplicantIncome = loan_req['ApplicantIncome']
+    LoanAmount = loan_req['LoanAmount'] / 1000
+
+    result = clf.predict([[Gender, Married, ApplicantIncome, LoanAmount, Credit_History]])
+
+    if result == 0:
+        pred = "Rejected"
+    else:
+        pred = "Approved"
+
+    return {"loan_approval_status": pred}
